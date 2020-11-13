9ff48d3df96acfb34f8128ee4728d00783b76386
Sarah Teichman <steichman18@amherst.edu>
2020-11-12 21:44:10 -0800

    adding optional file path

--- R/check_missingness.R
+++ R/check_missingness.R
@@ -3,11 +3,12 @@
 #'
 #' @param gene_names A list of names of genes.
 #' @param tip_names A list of names of genomes (tips of the phylogeny).
+#' @param path The path to the fasta file.
 #'
 #' @return A matrix with the proportion of missingness for each genome and gene combination.
 #'
 #' @export
-check_missingness <- function(gene_names, tip_names) {
+check_missingness <- function(gene_names, tip_names, path = "") {
   miss_prop <- matrix(data = NA, nrow = length(gene_names), ncol = length(tip_names))
   rownames(miss_prop) <- gene_names
   colnames(miss_prop) <- tip_names
@@ -16,7 +17,7 @@ check_missingness <- function(gene_names, tip_names) {
   command2 <- "while read i; do echo $i | grep -o '-'| wc -l; done <"
   indices <- (1:length(tip_names))*2
   for (i in 1:length(gene_names)) {
-    filename <- paste0(gene_names[i], ".fa")
+    filename <- paste0(path, gene_names[i], ".fa")
     full_command <- paste0(command1, filename)
     tot <- readr::parse_number(system(full_command, intern = T))
     full_command <- paste0(command2, filename)
