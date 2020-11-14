d54c75ac52040c58e7f084c9bbff40bea00bbbff
Sarah Teichman <steichman18@amherst.edu>
2020-11-14 15:58:55 -0800

    printing

--- R/compute_geodesic.R
+++ R/compute_geodesic.R
@@ -8,6 +8,7 @@ compute_geodesic <- function(tree_path) {
   output <- paste0("-o gtp_output.txt")
   #tree_path <- paste0("data/simple_bhv/example",seed,".txt")
   code_path <- system.file("inst/java", "gtp.jar", package = "TreeVizPackage")
+  print(code_path)
   system2('java',
           args = c('-jar', code_path, "-d", output,
                    tree_path),
