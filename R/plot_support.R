9d16100f911a48d1430f1268ce43604886b57630
Sarah Teichman <steichman18@amherst.edu>
2021-04-04 18:21:30 -0700

    updating plot_support to work with unrooted trees

--- R/plot_support.R
+++ R/plot_support.R
@@ -4,15 +4,18 @@
 #' @param main_tree A tree to check support for.
 #' @param n_trees The number of trees providing support for main_tree.
 #' @param branch_support A list of branch support values for each internal branch of main_tree.
+#' @param rooted A boolean, TRUE if the main tree is rooted
 #'
 #' @return A ggtree object.
 #'
 #' @import ggtree
 #'
 #' @export
-plot_support <- function(main_tree, n_trees, branch_support) {
+plot_support <- function(main_tree, n_trees, branch_support, rooted = TRUE) {
   n_tips <- length(main_tree$tip.label)
-  support_val <- round(c(rep(n_trees,(n_tips + 2)),branch_support)/n_trees,2)
+  num <- n_tips + 2
+  if (!rooted) {num <- n_tips+1}
+  support_val <- round(c(rep(n_trees,num),branch_support)/n_trees,2)
   support_lab <- paste0(support_val)
   plot <- main_tree %>%
     ggtree +
