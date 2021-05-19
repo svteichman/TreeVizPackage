0c50beca8fe71f26e523b1997e047a4d8dfb8c6a
Sarah Teichman <steichman18@amherst.edu>
2021-05-18 17:31:47 -0700

    adding function to plot split support

--- R/plot_node_value.R
+++ R/plot_node_value.R
@@ -1,9 +1,8 @@
-#' Plot support
-#' Plot a tree with support values on each internal branch.
+#' Plot node value
+#' Plot a tree with node values on each internal node.
 #'
 #' @param main_tree A tree to check support for.
-#' @param n_trees The number of trees providing support for main_tree.
-#' @param branch_support A list of branch support values for each internal branch of main_tree.
+#' @param node_lab A list of node values for each internal node of main_tree.
 #' @param rooted A boolean, TRUE if the main tree is rooted
 #' @param lab_size The size of the tip labels, default is 2.
 #' @param supp_size The size of the support labels, default is 2.
@@ -14,18 +13,18 @@
 #' @import ggtree
 #'
 #' @export
-plot_support <- function(main_tree, n_trees, branch_support, rooted = TRUE,
+plot_node_value <- function(main_tree, node_lab, rooted = TRUE,
                          lab_size = 2, supp_size = 2, xlim_max = 0.5) {
   n_tips <- length(main_tree$tip.label)
   num <- n_tips + 2
   if (!rooted) {num <- n_tips+1}
   #support_val <- round(c(rep(n_trees,num),branch_support)/n_trees,2)
-  support_val <- round(c(rep(NA,num),branch_support)/n_trees,2)
-  support_lab <- paste0(support_val)
+  node_val <- round(c(rep(NA,num),node)/n_trees,2)
+  node_lab <- paste0(node_val)
   plot <- main_tree %>%
     ggtree +
     geom_tiplab(size = lab_size) +
-    geom_text2(aes(subset=!isTip, label=support_lab, color = support_val),
+    geom_text2(aes(subset=!isTip, label=node_lab, color = node_val),
                hjust=1, vjust = -0.7, size = supp_size) +
     scale_colour_gradient(low = "red", high = "blue") +
     theme(legend.position = 'none') +
